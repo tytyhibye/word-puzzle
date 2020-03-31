e49d85afa1f4c89fbf186db60b1b22b64adb5e27
Mark Gridley <mark_gridley@yahoo.com>
2020-03-31 09:21:55 -0700

    add button to index.html, add content to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
-
-  
+  $(".").submit(function(event) {
+  event.preventDefault();
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  vowels.forEach(function(vowel) {
+  replace(' - ');
+  $(#answer).toggle()
+  $(#question).fadetoggle()
+    });
+  });
 });
